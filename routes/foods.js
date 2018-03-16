035eaa9504a46088e053967f3c65f8cc89b2e888
ThanhSon <thanhsoncun@gmail.com>
2018-03-16 22:56:26 +0700

    Update

--- routes/foods.js
+++ routes/foods.js
@@ -5,10 +5,10 @@ var	foodHandlers = require('../controllers/foodController.js');
 /* GET home page. */
 router.get('/:categoryId',userHandlers.loginRequired, foodHandlers.get);
 router.post('/:categoryId/create',userHandlers.loginRequired, foodHandlers.create);
-// router.post('/:categoryId/update/:foodId',userHandlers.loginRequired, foodHandlers.update);
+router.post('/:categoryId/update/:foodId',userHandlers.loginRequired, foodHandlers.update);
 // router.post('/:categoryId/delete/:foodId',userHandlers.loginRequired, foodHandlers.delete);
 
-router.get('/:foodId',userHandlers.loginRequired, foodHandlers.getDetail);
-router.post('/:foodId/updateFavourite',userHandlers.loginRequired, foodHandlers.updateFavourite);
+router.get('/:foodId/detail',userHandlers.loginRequired, foodHandlers.getDetail);
+
 
 module.exports = router;
