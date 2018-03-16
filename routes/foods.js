d6956964fa237858d38afd3af2529ffd8223b376
ThanhSon <thanhsoncun@gmail.com>
2018-03-16 22:53:37 +0700

    update Food

--- routes/foods.js
+++ routes/foods.js
@@ -9,6 +9,6 @@ router.post('/:categoryId/create',userHandlers.loginRequired, foodHandlers.creat
 // router.post('/:categoryId/delete/:foodId',userHandlers.loginRequired, foodHandlers.delete);
 
 router.get('/:foodId',userHandlers.loginRequired, foodHandlers.getDetail);
-router.post('/:categoryId/updateFavourite',userHandlers.loginRequired, foodHandlers.updateFavourite);
+router.post('/:foodId/updateFavourite',userHandlers.loginRequired, foodHandlers.updateFavourite);
 
 module.exports = router;
