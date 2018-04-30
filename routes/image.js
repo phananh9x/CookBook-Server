e6eac942ab8eaf18bf48068584c41bc1957febaa
Anh Phan <phananh123qqq@gmail.com>
2018-04-30 10:04:46 +0700

    Update path image

--- routes/image.js
+++ routes/image.js
@@ -5,10 +5,10 @@ var multer  = require('multer');
 
 var storage = multer.diskStorage({
     destination: (req, file, cb) => {
-      cb(null, 'public/images/uploads')
+      	cb(null, 'public/images/uploads')
     },
     filename: (req, file, cb) => {
-      cb(null, file.fieldname + '-' + Date.now())
+	    cb(null, file.fieldname + '-' + Date.now() + '-' + file.originalname)
     }
 });
 var upload = multer({storage: storage});
