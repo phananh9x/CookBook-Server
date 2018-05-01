b8ee118784ead60b5b1e0ed43332c72af7b8991b
ThanhSon <thanhsoncun@gmail.com>
2018-05-01 09:38:30 +0700

    Update

--- controllers/userController.js
+++ controllers/userController.js
@@ -8,9 +8,9 @@ exports.register = function(req, res) {
   var newUser = new User(req.body);
   newUser.hash_password = bcrypt.hashSync(req.body.password, 10);
   newUser.save(function(err, user) {
-    if (err) 
+    if (err)
       return res.status(500).send({
-        success: false, 
+        success: false,
         results: null,
         message: err
       });
@@ -23,33 +23,33 @@ exports.sign_in = function(req, res) {
   User.findOne({
     email: req.body.email
   }, function(err, user) {
-    if (err) 
+    if (err)
       return res.status(500).send({
-        success: false, 
+        success: false,
         results: null,
         message: err
       });
     if (!user || !user.comparePassword(req.body.password)) {
       return res.status(500).send({
-        success: false, 
+        success: false,
         results: null,
         message: 'Authentication failed. Invalid user or password.'
       });
     }
-    return res.send({success: true, results: { 
-      token: jwt.sign({ email: user.email, fullName: user.fullName, _id: user._id }, 
+    return res.send({success: true, results: {
+      token: jwt.sign({ email: user.email, fullName: user.fullName, _id: user._id, phone: user.phone, address: user.address }, 
       config.secret) ,
-      fullName:user.fullName, 
-      _id: user._id, 
+      fullName:user.fullName,
+      _id: user._id,
       email: user.email}});
   });
 };
 
 exports.update = function(req, res) {
   User.findOneAndUpdate({_id : req.params.userId}, req.body, {new: true}, function (err, data) {
-    if (err) 
+    if (err)
       return res.status(400).send({
-        success: false, 
+        success: false,
         results: null,
         message: err
       });
@@ -60,9 +60,9 @@ exports.update = function(req, res) {
 
 exports.delete = function(req, res) {
   User.findByIdAndRemove({_id : req.params.userId}, function (err, data) {
-    if (err) 
+    if (err)
       return res.status(400).send({
-        success: false, 
+        success: false,
         results: null,
         message: err
       });
@@ -77,10 +77,9 @@ exports.loginRequired = function(req, res, next) {
     next();
   } else {
     return res.status(500).send({
-      success: false, 
+      success: false,
       results: null,
       message: 'Unauthorized user!'
     });
   }
 };
-
